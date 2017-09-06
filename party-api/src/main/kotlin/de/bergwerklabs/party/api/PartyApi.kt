2e0e8abab3dfc91f847e682551b35d73276ff825
freggy <freggy@bergwerklabs.de>
2017-09-06 19:49:43 +0200

    Initial commit

---
+++ party-api/src/main/kotlin/de/bergwerklabs/party/api/PartyApi.kt
@@ -0,0 +1,16 @@
+package de.bergwerklabs.party.api
+
+/**
+ * Created by Yannic Rieger on 06.09.2017.
+ * <p>
+ * @author Yannic Rieger
+ */
+class PartyApi {
+    
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+        
+        }
+    }
+}
---
+++ party-server/src/main/kotlin/de/bergwerklabs/party/server/PartyServer.kt
@@ -0,0 +1,15 @@
+package de.bergwerklabs.party.server
+
+/**
+ * Created by Yannic Rieger on 06.09.2017.
+ * <p>
+ * @author Yannic Rieger
+ */
+class PartyServer {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+        
+        }
+    }
+}
